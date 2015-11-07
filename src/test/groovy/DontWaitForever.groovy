7aa4000ace53e890ed94b1c140eaed31889b305f
Dmitry Ledentsov <dmlled@yahoo.com>
2015-11-07 22:39:12 +0100

    some formatting

--- src/test/groovy/DontWaitForever.groovy
+++ src/test/groovy/DontWaitForever.groovy
@@ -8,21 +8,24 @@ import java.util.concurrent.TimeUnit
 class DontWaitForever extends Specification {
     def "why wait?"() {
         setup:
-        	def scheduler = new TestScheduler()
-        	def observable = Observable.just(1).delay(100, TimeUnit.DAYS, scheduler)
-        	def done = false
+            def scheduler = new TestScheduler()
+
+            // system under test: will tick once after a hundred days
+            def observable = Observable.just(1).delay(100, TimeUnit.DAYS, scheduler)
+            def done = false
 
         when:
-        	observable.subscribe {
-        		done = true
-        	}
+            observable.subscribe {
+                done = true
+            }
 
-        	done == false
+            // still in the initial state
+            done == false
 
         and:
-        	scheduler.advanceTimeBy(100, TimeUnit.DAYS)
+            scheduler.advanceTimeBy 100, TimeUnit.DAYS
 
         then:
-        	done == true
+            done == true
     }
 }
