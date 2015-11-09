d3c2a74c7623049edec039dc294bc506c8f071b7
Dmitry Ledentsov <dmlled@yahoo.com>
2015-11-09 21:30:33 +0100

    Update WaitSome.groovy

--- src/main/groovy/WaitSome.groovy
+++ src/main/groovy/WaitSome.groovy
@@ -1,16 +1,20 @@
 import rx.*
 import java.util.concurrent.TimeUnit
 
+// system under test: should tick once after 5 seconds
 def observable = Observable
 	.just(1)
 	.delay(5, TimeUnit.SECONDS)
 
-observable.subscribe { println 'ah, OK, done! Or not?' }
+// the desired side-effect
+observable.subscribe {println 'ah, OK, done! Or not?'}
 
+// the test
 Observable
 	.interval(1,TimeUnit.SECONDS)
-	.subscribe { println 'still waiting...' }
+	.subscribe {println 'still waiting...'}
 
 println 'starting to wait for the test to complete ...'
 
+// wait for completion
 observable.toBlocking().last()
